c97e768bcbc49cf9d97d8898ab409312565dd39e
unknown <amincxo@gmail.com>
2025-05-05 15:56:00 +0330

    add styles and install mongoose and layout

---
+++ components/layout/Layout.js
@@ -0,0 +1,20 @@
+import Link from "next/link"
+
+function Layout({children}) {
+  return (
+    <>
+        <header className="header" >
+            <h2>amin crm</h2>
+            <Link href="add-customer" >Add Customer</Link>
+        </header>
+        <div className="main" >
+            {children}
+        </div>
+        <footer className="footer" >
+            <a href="aminborvayeh.ir" target="_blank" rel="noreferrer" >aminborvayeh</a> next js cource CRM &copy;
+        </footer>
+    </>
+  )
+}
+
+export default Layout
