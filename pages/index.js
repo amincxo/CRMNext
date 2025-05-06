aec70d7171952d56f920996d375e16eaf08ab825
unknown <amincxo@gmail.com>
2025-05-06 18:31:16 +0330

    index page completed and template home and card under develope

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
+import connectDB from "../utils/connectDB"
+import Customer from "../models/Customer"
+import HomePage from "../components/template/HomePage";
 
-export default function Home() {
-  return (
-    <>
+export default function Index({customers}) {
+  return <HomePage customers={customers} />
+}
 
-    </>
-  )
+export async function getServerSideProps() {
+    try{
+        await connectDB();
+        const customers = await Customer.find();
+        return {
+            props: {
+                customers: JSON.parse(JSON.stringify(customers))
+            }
+        }
+    }catch (err) {
+        return {
+            notFound: true
+        }
+    }
 }
