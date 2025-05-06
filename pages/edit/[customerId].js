f9018b3721e3937d5660b853a6c4236c6d4a9b9e
unknown <amincxo@gmail.com>
2025-05-06 21:07:32 +0330

    editpage console comment complate and connected to api for edit client

--- pages/edit/[customerId].js
+++ pages/edit/[customerId].js
@@ -1,9 +1,20 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import CustomerEditPage from '../../components/template/customerEditPage'
+import { useRouter } from 'next/router';
 
 function Index() {
-  return (
-    <div>Index</div>
-  )
+    const [data , setData] = useState(null);
+
+    const router = useRouter();
+    const {query: {customerId} , isReady} = router;
+
+    useEffect(()=>{
+        if (isReady) {
+            fetch(`/api/customer/${customerId}`).then(res => res.json()).then(data => setData(data.data))
+        }
+    },[isReady])
+    if (data) return <CustomerEditPage data={data} id={customerId} />
+    
 }
 
 export default Index
